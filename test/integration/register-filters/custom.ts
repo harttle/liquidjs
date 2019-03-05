998832c7723248308332c5e815d02c1434a2967a
harttle <yangjvn@126.com>
2019-03-06 01:39:11 +0800

    refactor: mv custom filter test

---
+++ test/integration/register-filters/custom.ts
@@ -0,0 +1,17 @@
+import { test, liquid } from '../../stub/render'
+
+describe('registerFilter', function () {
+  describe('object arguments', function () {
+    liquid.registerFilter('obj_test', function () {
+      return JSON.stringify(arguments)
+    })
+    it('should support object', () => test(
+      `{{ "a" | obj_test: k1: "v1", k2: foo }}`,
+      '{"0":"a","1":["k1","v1"],"2":["k2","bar"]}'
+    ))
+    it('should support mixed object', () => test(
+      `{{ "a" | obj_test: "something", k1: "v1", k2: foo }}`,
+      '{"0":"a","1":"something","2":["k1","v1"],"3":["k2","bar"]}'
+    ))
+  })
+})
