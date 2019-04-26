6075c0a54f5ca2f995f7f86c76d1ff7d419ec96b
harttle <yangjvn@126.com>
2019-04-27 00:26:17 +0800

    fix: `default` filter is not working with an empty string, #122

--- src/builtin/filters/object.ts
+++ src/builtin/filters/object.ts
@@ -1,5 +1,5 @@
-import { isTruthy } from '../../render/syntax'
+import { isFalsy } from '../../render/syntax'
 
 export default {
-  'default': <T1, T2>(v: T1, arg: T2): T1 | T2 => isTruthy(v) ? v : arg
+  'default': <T1, T2>(v: string | T1, arg: T2): string | T1 | T2 => isFalsy(v) || v === '' ? arg : v
 }
--- test/integration/builtin/filters/object.ts
+++ test/integration/builtin/filters/object.ts
@@ -2,7 +2,12 @@ import { test } from '../../../stub/render'
 
 describe('filters/object', function () {
   describe('default', function () {
-    it('should use default when falsy', () => test('{{false |default: "a"}}', 'a'))
-    it('should not use default when truthy', () => test('{{true |default: "a"}}', 'true'))
+    it('false should use default', () => test('{{false | default: "a"}}', 'a'))
+    it('empty string should use default', () => test('{{"" | default: "a"}}', 'a'))
+    it('non-empty string should not use default', () => test('{{" " | default: "a"}}', ' '))
+    it('nil should use default', () => test('{{nil | default: "a"}}', 'a'))
+    it('undefined should use default', () => test('{{not_defined | default: "a"}}', 'a'))
+    it('true should not use default', () => test('{{true | default: "a"}}', 'true'))
+    it('0 should not use default', () => test('{{0 | default: "a"}}', '0'))
   })
 })
