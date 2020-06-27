ff465772bc9466372df6813be9e6dbe7f60e255d
Tejas Manohar <me@tejas.io>
2020-06-27 14:47:07 -0700

    cleanup

--- src/util/collection.ts
+++ src/util/collection.ts
@@ -1,11 +1,6 @@
 import { isString, isObject, isArray } from './underscore'
-import {isThenable} from "./async"
 
-export function * toEnumerable (val: any): any {
-  if (isThenable(val)) {
-    val = yield val
-  }
-
+export function toEnumerable (val: any) {
   if (isArray(val)) return val
   if (isString(val) && val.length > 0) return [val]
   if (isObject(val)) return Object.keys(val).map((key) => [key, val[key]])
