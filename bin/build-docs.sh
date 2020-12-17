c8db5e625967ced378d3648441f1fcdc174a8610
harttle <yangjvn@126.com>
2020-12-17 23:08:54 +0800

    docs: try fix special chars in changelog.md

--- bin/build-docs.sh
+++ bin/build-docs.sh
@@ -9,8 +9,16 @@ npm run build
 cp CNAME public/
 
 cp ../CHANGELOG.md source/tutorials/changelog.md
+sed -i \
+  -e 's/{%/{% raw %}{%{% endraw %}/g' \
+  -e 's/{{/{% raw %}{{{% endraw %}/g' \
+  -e '1 s/"/\&quot;/g' \
+  -e '1 s/</\&lt;/g' \
+  -e '1 s/>/\&gt;/g' \
+  source/tutorials/changelog.md
+cp source/tutorials/changelog.md source/zh-cn/tutorials/changelog.md
+
 sed -i '1i ---\ntitle: Changelog\nauto: true\n---\n' source/tutorials/changelog.md
-cp ../CHANGELOG.md source/zh-cn/tutorials/changelog.md
 sed -i '1i ---\ntitle: 更新日志\nauto: true\n---\n' source/zh-cn/tutorials/changelog.md
 
 if [ "$HEXO_ALGOLIA_INDEXING_KEY" != "" ]; then
