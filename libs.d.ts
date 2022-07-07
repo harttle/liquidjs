ed95d9a760e3579b13df998e3f8aee68c07e5c86
Harttle <harttle@harttle.com>
2022-07-08 02:56:39 +0800

    chore: update to node 16

--- libs.d.ts
+++ libs.d.ts
@@ -1,5 +1,5 @@
 declare module 'rollup-plugin-uglify' {
-  export function uglify (): any
+  export function uglify (options?: any): any
 }
 declare module 'rollup-plugin-replace' {
   export default function replace(options: any): any
