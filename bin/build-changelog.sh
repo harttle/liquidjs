8ea84058f29437598d02833148fb841efe61d261
Tomas Hubelbauer <tomas@hubelbauer.net>
2023-05-28 21:25:52 +0200

    fix: add macOS branches to the shell scripts
    The sed command line was difficult (impossible?) to be made portable so I just added macOS conditions.

--- bin/build-changelog.sh
+++ bin/build-changelog.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
+REPLACE_START_RAW='s/{%/{% raw %}{%{% endraw %}/g'
+REPLACE_END_RAW='s/{{/{% raw %}{{{% endraw %}/g'
+ESCAPE_QUOT='1 s/"/\&quot;/g'
+ESCAPE_LT='1 s/</\&lt;/g'
+ESCAPE_GT='1 s/>/\&gt;/g'
+
 cd docs
 cp ../CHANGELOG.md source/tutorials/changelog.md
-sed -i '' \
-  -e 's/{%/{% raw %}{%{% endraw %}/g' \
-  -e 's/{{/{% raw %}{{{% endraw %}/g' \
-  -e '1 s/"/\&quot;/g' \
-  -e '1 s/</\&lt;/g' \
-  -e '1 s/>/\&gt;/g' \
-  source/tutorials/changelog.md
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' -e "$REPLACE_START_RAW" -e "$REPLACE_END_RAW" -e "$ESCAPE_QUOT" -e "$ESCAPE_LT" -e "$ESCAPE_GT" source/tutorials/changelog.md
+else
+    sed -i -e "$REPLACE_START_RAW" -e "$REPLACE_END_RAW" -e "$ESCAPE_QUOT" -e "$ESCAPE_LT" -e "$ESCAPE_GT" source/tutorials/changelog.md
+fi
 cp source/tutorials/changelog.md source/zh-cn/tutorials/changelog.md
 
-sed -i '' \
-  -e '1i\
- ---\ntitle: Changelog\nauto: true\n---\n' \
-  source/tutorials/changelog.md
-
-sed -i '' \
-  -e '1i\
- ---\ntitle: 更新日志\nauto: true\n---\n' \
-  source/zh-cn/tutorials/changelog.md
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' -e '1i\
+ ---\ntitle: Changelog\nauto: true\n---\n' source/tutorials/changelog.md
+    sed -i '' -e '1i\
+ ---\ntitle: 更新日志\nauto: true\n---\n' source/zh-cn/tutorials/changelog.md
+else
+    sed -i '1i ---\ntitle: Changelog\nauto: true\n---\n' source/tutorials/changelog.md
+    sed -i '1i ---\ntitle: 更新日志\nauto: true\n---\n' source/zh-cn/tutorials/changelog.md
+fi
--- bin/build-contributors.sh
+++ bin/build-contributors.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+REPLACE_README_MD='s/README.md/docs\/themes\/navy\/layout\/partial\/all-contributors.swig/g'
+REPLACE_CONTRIBUTORS_PER_LINE='s/"contributorsPerLine": 7/"contributorsPerLine": 65535/g'
+REPLACE_CONTRIBUTOR_LINKS='s/<br \/>.*<\/td>/<\/a><\/td>/g'
+
 cp .all-contributorsrc docs/.all-contributorsrc
-sed -i '' \
-    -e 's/README.md/docs\/themes\/navy\/layout\/partial\/all-contributors.swig/g' \
-    -e 's/"contributorsPerLine": 7/"contributorsPerLine": 65535/g' \
-    docs/.all-contributorsrc
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' -e "$REPLACE_README_MD" -e "$REPLACE_CONTRIBUTORS_PER_LINE" docs/.all-contributorsrc
+else
+    sed -i -e "$REPLACE_README_MD" -e "$REPLACE_CONTRIBUTORS_PER_LINE" docs/.all-contributorsrc
+fi
 
 all-contributors --config docs/.all-contributorsrc generate
-sed -i '' \
-    -e 's/<br \/>.*<\/td>/<\/a><\/td>/g' docs/themes/navy/layout/partial/all-contributors.swig
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' -e "$REPLACE_CONTRIBUTOR_LINKS" docs/themes/navy/layout/partial/all-contributors.swig
+else
+    sed -i "$REPLACE_CONTRIBUTOR_LINKS" docs/themes/navy/layout/partial/all-contributors.swig
+fi
