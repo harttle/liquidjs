63eb8d2d4783224b806f5d9332f8cc9d7a622d92
Tomas Hubelbauer <tomas@hubelbauer.net>
2023-05-29 19:31:24 +0200

    fix: use 2 spaces like the original scripts did
    Not sure why VS Code went with 4.

--- bin/build-changelog.sh
+++ bin/build-changelog.sh
@@ -2,30 +2,30 @@
 
 # Run `sed` in a way that's compatible with both macOS (BSD) and Linux (GNU)
 sedi() {
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        sed -i '' "$@"
-    else
-        sed -i "$@"
-    fi
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "$@"
+  else
+    sed -i "$@"
+  fi
 }
 
 cd docs
 cp ../CHANGELOG.md source/tutorials/changelog.md
 sedi \
-    -e 's/{%/{% raw %}{%{% endraw %}/g' \
-    -e 's/{{/{% raw %}{{{% endraw %}/g' \
-    -e '1 s/"/\&quot;/g' \
-    -e '1 s/</\&lt;/g' \
-    -e '1 s/>/\&gt;/g' \
-    source/tutorials/changelog.md
+  -e 's/{%/{% raw %}{%{% endraw %}/g' \
+  -e 's/{{/{% raw %}{{{% endraw %}/g' \
+  -e '1 s/"/\&quot;/g' \
+  -e '1 s/</\&lt;/g' \
+  -e '1 s/>/\&gt;/g' \
+  source/tutorials/changelog.md
 cp source/tutorials/changelog.md source/zh-cn/tutorials/changelog.md
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' -e '1i\
+  sed -i '' -e '1i\
  ---\ntitle: Changelog\nauto: true\n---\n' source/tutorials/changelog.md
-    sed -i '' -e '1i\
+  sed -i '' -e '1i\
  ---\ntitle: 更新日志\nauto: true\n---\n' source/zh-cn/tutorials/changelog.md
 else
-    sed -i '1i ---\ntitle: Changelog\nauto: true\n---\n' source/tutorials/changelog.md
-    sed -i '1i ---\ntitle: 更新日志\nauto: true\n---\n' source/zh-cn/tutorials/changelog.md
+  sed -i '1i ---\ntitle: Changelog\nauto: true\n---\n' source/tutorials/changelog.md
+  sed -i '1i ---\ntitle: 更新日志\nauto: true\n---\n' source/zh-cn/tutorials/changelog.md
 fi
--- bin/build-contributors.sh
+++ bin/build-contributors.sh
@@ -2,18 +2,18 @@
 
 # Run `sed` in a way that's compatible with both macOS (BSD) and Linux (GNU)
 sedi() {
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        sed -i '' "$@"
-    else
-        sed -i "$@"
-    fi
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "$@"
+  else
+    sed -i "$@"
+  fi
 }
 
 cp .all-contributorsrc docs/.all-contributorsrc
 sedi \
-    -e 's/README.md/docs\/themes\/navy\/layout\/partial\/all-contributors.swig/g' \
-    -e 's/"contributorsPerLine": 7/"contributorsPerLine": 65535/g' \
-    docs/.all-contributorsrc
+  -e 's/README.md/docs\/themes\/navy\/layout\/partial\/all-contributors.swig/g' \
+  -e 's/"contributorsPerLine": 7/"contributorsPerLine": 65535/g' \
+  docs/.all-contributorsrc
 
 all-contributors --config docs/.all-contributorsrc generate
 sedi 's/<br \/>.*<\/td>/<\/a><\/td>/g' docs/themes/navy/layout/partial/all-contributors.swig
