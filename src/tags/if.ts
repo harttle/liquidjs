c5d678c4916bab14aa97b6284cd6633551ec7f5b
Jun Yang <harttleharttle@gmail.com>
2024-02-18 13:18:26 +0800

    chore: Update src/tags/if.ts

--- src/tags/if.ts
+++ src/tags/if.ts
@@ -29,7 +29,7 @@ export default class extends Tag {
       })
       .on('tag:endif', function () { this.stop() })
       .on('template', (tpl: Template) => {
-        if (p !== this.elseTemplates || (p === this.elseTemplates && elseCount === 1)) {
+        if (p !== this.elseTemplates || elseCount === 1) {
           p.push(tpl)
         }
       })
