c278200c18524544aae53ab471d0923f03f7b8e0
Adam Tanner <adm@admtnnr.com>
2024-05-18 11:08:04 -0400

    fix: allow liquidMethodMissing to return any supported value type

--- src/drop/drop.ts
+++ src/drop/drop.ts
@@ -1,5 +1,5 @@
 export abstract class Drop {
-  public liquidMethodMissing (key: string | number): Promise<string | undefined> | string | undefined {
+  public liquidMethodMissing (key: string | number): Promise<any | undefined> | any | undefined {
     return undefined
   }
 }
