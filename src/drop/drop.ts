271e6ee7d6568ced4236e264d50a916dae312696
Adam Tanner <27502+admtnnr@users.noreply.github.com>
2024-05-19 07:54:52 -0400

    Update src/drop/drop.ts
    Co-authored-by: Jun Yang <harttleharttle@gmail.com>

--- src/drop/drop.ts
+++ src/drop/drop.ts
@@ -1,5 +1,5 @@
 export abstract class Drop {
-  public liquidMethodMissing (key: string | number): Promise<any | undefined> | any | undefined {
+  public liquidMethodMissing (key: string | number): Promise<any> | any {
     return undefined
   }
 }
