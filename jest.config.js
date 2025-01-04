5b7429ae040f1163c7d38b4bd6185eea70d6ec30
Harttle <yangjvn@126.com>
2025-01-04 23:29:11 +0800

    fix: enumerate plain objects in where/where_exp

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  maxWorkers: '50%',
   testEnvironment: 'node',
   testMatch: ['**/*.spec.ts'],
   collectCoverageFrom: [
