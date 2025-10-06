cd0b1e4cedf204990be2fdd7d12af7fa094a18cf
Yang Jun <jun.yang@airwallex.com>
2025-10-06 18:15:31 +0800

    feat: allow context access in liquidMethodMissing, #808

--- src/drop/drop.ts
+++ src/drop/drop.ts
@@ -1,5 +1,7 @@
+import { Context } from '../context'
+
 export abstract class Drop {
-  public liquidMethodMissing (key: string | number): Promise<any> | any {
+  public liquidMethodMissing (key: string | number, context: Context): Promise<any> | any {
     return undefined
   }
 }
--- test/e2e/drop.spec.ts
+++ test/e2e/drop.spec.ts
@@ -1,25 +1,37 @@
-import { Drop, Liquid } from '../..'
-
-class SettingsDrop extends Drop {
-  public foo = 'FOO'
-  public bar () {
-    return 'BAR'
-  }
-  public liquidMethodMissing (key: string) {
-    return key.toUpperCase()
-  }
-}
+import { Context, Drop, Liquid } from '../..'
 
 describe('drop', function () {
-  const settings = new SettingsDrop()
   let engine: Liquid
   beforeEach(function () {
     engine = new Liquid()
   })
-  it('should support liquidMethodMissing', async function () {
-    const src = `{{settings.foo}},{{settings.bar}},{{settings.coo}}`
-    const html = await engine.parseAndRender(src, { settings })
-    return expect(html).toBe('FOO,BAR,COO')
+
+  describe('liquidMethodMissing', () => {
+    it('should support liquidMethodMissing', async function () {
+      class SettingsDrop extends Drop {
+        public foo = 'FOO'
+        public bar () {
+          return 'BAR'
+        }
+        public liquidMethodMissing (key: string) {
+          return key.toUpperCase()
+        }
+      }
+      const src = `{{settings.foo}},{{settings.bar}},{{settings.coo}}`
+      const html = await engine.parseAndRender(src, { settings: new SettingsDrop() })
+      return expect(html).toBe('FOO,BAR,COO')
+    })
+
+    it('should expose context', async function () {
+      class SettingsDrop extends Drop {
+        public liquidMethodMissing (key: string, context: Context) {
+          return key + ':' + context.getSync([key])
+        }
+      }
+      const src = `{{settings.foo}}`
+      const html = await engine.parseAndRender(src, { settings: new SettingsDrop(), foo: 'FOO' })
+      return expect(html).toBe('foo:FOO')
+    })
   })
 
   describe('BlandDrop', function () {
